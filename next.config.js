ec8b67994c0c66a673363df4b291b653990cb0e4
harsh.davda <harsh.davda@iflair.com>
2022-06-20 16:19:19 +0530

    done setup of apolloclient frontend side

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const dotenv = require("dotenv");
+const res = dotenv.config({ path: "./.env" });
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
+module.exports = {
+  env: {
+    ENV: res.parsed.ENV,
+    BACKEND_URL: res.parsed.BACKEND_URL,
+    API_URL: res.parsed.API_URL,
+  },
+  nextConfig,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { gql, useQuery } from "@apollo/client";
+import * as React from "react";
+
+export const Pokemon = gql`
+  query {
+    pokemon {
+      id
+      type
+      name
+      pokedex
+    }
+  }
+`;
 
 const Home: NextPage = () => {
+  const pokemonData = useQuery(Pokemon);
+
+  const pokemon = React.useMemo(() => {
+    return pokemonData?.data;
+  }, [pokemonData?.data]);
   return (
     <div className={styles.container}>
       <Head>
@@ -18,38 +36,25 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {pokemon?.pokemon.map((i:any ) => {
+            return (
+             
+                <a href="https://nextjs.org/docs" className={styles.card} key={i.id}>
+                  <h2>{i.name}</h2>
+                  <p>
+                    {i.type}
+                  </p>
+                  <p>
+                    {i.pokedex}
+                  </p>
+                </a>
+            );
+          })}
         </div>
       </main>
 
@@ -59,14 +64,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
